8d7ddf7319d4e0b0a5e6a34fc6c94ce739ce4b78
Wouter Visser <fusion86@cerbus.nl>
2019-07-03 13:01:07 +0200

    Damn AppleScript is actually useful

--- spotify-gloves.py
+++ spotify-gloves.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
 import sys
-from spotify_local import SpotifyLocal
+import osascript
 
-spotify = SpotifyLocal()
-is_paused = True
+# Thanks to https://gist.github.com/NoobsArePeople2/5121597
+# and https://coolaj86.com/articles/how-to-control-os-x-system-volume-with-applescript/
+as_pause = 'tell application "Spotify"\nplaypause\nend tell'
+as_volume_up = "set volume output volume (output volume of (get volume settings) + 5)"
+as_volume_down = "set volume output volume (output volume of (get volume settings) - 5)"
 
 for xline in sys.stdin:
     line = xline.rstrip()
     print(line)
 
-    if ("action:" in line):
+    if "action:" in line:
         action = line.split(":")[1].strip()
         print("FOUND ACTION: " + action)
-        if ("toggle_play_pause" in action):
-            spotify.pause(is_paused)
-            is_paused = not is_paused
-        elif ("volume_up" in action):
-            pass
-        elif ("volume_down" in action):
-            pass
+        if "toggle_play_pause" in action:
+            osascript.run(as_pause)
+        elif "volume_up" in action:
+            osascript.run(as_volume_up)
+        elif "volume_down" in action:
+            osascript.run(as_volume_down)
